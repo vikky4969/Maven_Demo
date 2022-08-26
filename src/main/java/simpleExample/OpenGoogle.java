39dfbc008457beddea1d5e499d3c42078333739d
vikky4969 <mrvignesh.vikky9@gmail.com>
2022-08-26 14:07:06 +0530

    initial commit

---
+++ src/main/java/simpleExample/OpenGoogle.java
@@ -0,0 +1,15 @@
+package simpleExample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenGoogle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SATHISH\\eclipse-workspace\\SeleniumPractice\\Drivers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+	}
+
+}
